e76f3211bbda6b2a22ae647c70ca5c17dea7a1c8
redcliff-op <devyashjangid@gmail.com>
2024-03-10 19:45:47 +0530

    Gradle: Update Firebase Auth and Firestore Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.gms:google-services:4.4.1")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
